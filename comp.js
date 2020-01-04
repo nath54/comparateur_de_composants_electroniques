6b5ad8558ce45cc902bc5bd584213605be800e71
nath54 <cerisara.nathan@gmail.com>
2020-01-04 14:27:20 +0100

    a

--- comp.js
+++ comp.js
@@ -6,83 +6,34 @@ function makePage(id){
         var div=document.createElement("div");
         var table=document.createElement("table");
         var lignes=[];
-        //ligne du nom
-        var ligne1=document.createElement("tr"); 
-        var l1c1=document.createElement("td");
-        var l1c2=document.createElement("td");
-        var pl1c1=document.createElement("p");
-        l1c1.setAttribute("class","c11");
-        l1c2.setAttribute("class","c21");
-        pl1c1.innerHTML="Nom";
-        var pl1c2=document.createElement("p");
-        pl1c2.innerHTML=comp.nom;
-        l1c1.appendChild(pl1c1);
-        l1c2.appendChild(pl1c2);
-        ligne1.appendChild(l1c1);
-        ligne1.appendChild(l1c2);
-        lignes.push(ligne1);
-        //ligne de la marque
-        var ligne2=document.createElement("tr"); 
-        var l2c1=document.createElement("td");
-        var l2c2=document.createElement("td");
-        var pl2c1=document.createElement("p");
-        l2c1.setAttribute("class","c12");
-        l2c2.setAttribute("class","c22");
-        pl2c1.innerHTML="Marque";
-        var pl2c2=document.createElement("p");
-        pl2c2.innerHTML=comp.marque;
-        l2c1.appendChild(pl2c1);
-        l2c2.appendChild(pl2c2);
-        ligne2.appendChild(l2c1);
-        ligne2.appendChild(l2c2);
-        lignes.push(ligne2);
         //
         if(comp.type=="cpu"){
-            //ligne du socket
-            var ligne3=document.createElement("tr"); 
-            var l3c1=document.createElement("td");
-            var l3c2=document.createElement("td");
-            var pl3c1=document.createElement("p");
-            l3c1.setAttribute("class","c11");
-            l3c2.setAttribute("class","c21");
-            pl3c1.innerHTML="Socket";
-            var pl3c2=document.createElement("p");
-            pl3c2.innerHTML=comp.socket;      
-            l3c1.appendChild(pl3c1);
-            l3c2.appendChild(pl3c2);
-            ligne3.appendChild(l3c1);
-            ligne3.appendChild(l3c2);
-            lignes.push(ligne3);
-            //ligne de la micro architecture
-            var ligne4=document.createElement("tr"); 
-            var l4c1=document.createElement("td");
-            var l4c2=document.createElement("td");
-            var pl4c1=document.createElement("p");
-            l4c1.setAttribute("class","c12");
-            l4c2.setAttribute("class","c22");
-            pl4c1.innerHTML="Micro-Architecture";
-            var pl4c2=document.createElement("p");
-            pl4c2.innerHTML=comp.micro_architecture;
-            l4c1.appendChild(pl4c1);
-            l4c2.appendChild(pl4c2);
-            ligne4.appendChild(l4c1);
-            ligne4.appendChild(l4c2); 
-            lignes.push(ligne4);
-            //ligne du nombre de coeurs
-            var ligne5=document.createElement("tr"); 
-            var l5c1=document.createElement("td");
-            var l5c2=document.createElement("td");
-            var pl5c1=document.createElement("p");
-            l5c1.setAttribute("class","c11");
-            l5c2.setAttribute("class","c21");
-            pl5c1.innerHTML="Nombre de coeurs";
-            var pl5c2=document.createElement("p");
-            pl5c2.innerHTML=comp.nbcoeurs;      
-            l5c1.appendChild(pl5c1);
-            l5c2.appendChild(pl5c2);
-            ligne5.appendChild(l5c1);
-            ligne5.appendChild(l5c2);
-            lignes.push(ligne5);
+            var liste=[["Nom",comp.nom],["Marque",comp.marque],["Socket",comp.socket],["Micro-Architecture",comp.micro_architecture],["Nombre de coeurs",comp.nbcoeurs],["Nombre de threads",comp.nbthreads]];
+            tpc="1"
+            for(el of liste){
+                var ligne=document.createElement("tr");
+                var c1=document.createElement("td");
+                var c2=document.createElement("td");
+                if(tpc=="1"){
+                    tpc="2";
+                    c1.setAttribute("class","c11");
+                    c2.setAttribute("class","c21");
+                }
+                else{
+                    tpc="1";
+                    c1.setAttribute("class","c12");
+                    c2.setAttribute("class","c22");
+                }
+                var pc1=document.createElement("p");
+                var pc2=document.createElement("p");
+                pc1.innerHTML=el[0];
+                pc2.innerHTML=el[1];
+                c1.appendChild(pc1);
+                c2.appendChild(pc2);
+                ligne.appendChild(c1);
+                ligne.appendChild(c2);
+                lignes.push(ligne);
+            }
             //
         }
         //
